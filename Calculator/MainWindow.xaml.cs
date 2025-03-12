a1f4996b49921bffeef8c2d32df5d504ecf903c2
dan <dan.ignat@student.unitbv.ro>
2025-03-13 01:16:09 +0200

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Calculator.View;
+
+namespace Calculator {
+    public partial class MainWindow : Window {
+        private StandardView standardView;
+        private ProgrammerView programmerView;
+
+        public MainWindow() {
+            InitializeComponent();
+
+            standardView = new StandardView();
+            programmerView = new ProgrammerView();
+
+            CalculatorViewHost.Content = standardView;
+        }
+
+        private void CalculatorMode_Changed(object sender, RoutedEventArgs e) {
+            if (CalculatorViewHost == null)
+                return;
+
+            if (sender == StandardRadio && StandardRadio.IsChecked == true) {
+                CalculatorViewHost.Content = standardView;
+            }
+            else if (sender == ProgrammerRadio && ProgrammerRadio.IsChecked == true) {
+                CalculatorViewHost.Content = programmerView;
+            }
+        }
+    }
+}
